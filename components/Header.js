fbb539feeac075db46d7901f5adcc08d87e3a6c1
Joe Attardi <jattardi@gmail.com>
2020-12-19 21:56:35 -0500

    Add dark mode to body

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import NavMenu from './NavMenu';
 
 export default function Header() {
   return (
-    <header className={`bg-blue-50 dark:bg-header-pattern-dark bg-header-pattern p-4 flex flex-row items-center justify-center dark:text-white dark:bg-gray-800`}>
+    <header className="bg-blue-50 dark:bg-header-pattern-dark bg-header-pattern p-4 flex flex-row items-center justify-center dark:text-white dark:bg-gray-800 border-b border-blue-200 dark:border-gray-700">
       <div className="flex flex-col items-center">
         <img className="rounded-full w-20 h-20 bg-blue-200" src="/joe.png" />
         <h1 className="text-4xl uppercase font-light">Joe Attardi</h1>
