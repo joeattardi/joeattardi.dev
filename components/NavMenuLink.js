5fe11061e5b96d6f9d2c665d8e7e9797e097455c
Joe Attardi <jattardi@gmail.com>
2020-12-20 14:03:34 -0500

    Change dark mode nav background color

--- components/NavMenuLink.js
+++ components/NavMenuLink.js
@@ -4,7 +4,7 @@ export default function NavMenuLink({ children, href }) {
   return (
     <li className="text-center text-4xl lg:text-lg my-4 mx-1">
       <Link href={href}>
-        <a className="hover:text-white hover:bg-indigo-600 px-4 py-2 rounded transition">{children}</a>
+        <a className="hover:text-white hover:bg-indigo-600 dark:hover:bg-gray-500 px-4 py-2 rounded transition">{children}</a>
       </Link>
     </li>
   );
