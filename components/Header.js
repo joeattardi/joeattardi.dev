471a8a97ff3054ee588d039fff94e7f9eec6916a
Joe Attardi <jattardi@gmail.com>
2020-12-20 22:26:03 -0500

    Add scaled nav links, transparent header on homepage

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,25 @@
 import Link from 'next/link';
 
+import classNames from 'classnames';
+
 import NavMenu from './NavMenu';
 
-export default function Header() {
+export default function Header({ transparent }) {
+
+  const headerClasses = classNames(
+    'lg:p-2',
+    'p-4',
+    'dark:text-white',
+    {
+      'bg-blue-50': !transparent,
+      'dark:bg-gray-700': !transparent,
+      'dark:bg-header-pattern-dark': !transparent,
+      'bg-header-pattern': !transparent
+    }
+  )
+
   return (
-    <header className="bg-blue-50 dark:bg-header-pattern-dark bg-header-pattern lg:p-2 p-4 dark:text-white dark:bg-gray-700 border-b border-blue-200 dark:border-gray-700">
+    <header className={headerClasses}>
       <div className="flex flex-row items-center justify-center sm:justify-start max-w-screen-xl mx-auto">
         <div className="flex flex-col items-center sm:flex-row flex-grow sm:space-x-4">
           <Link href="/"><a><img className="rounded-full lg:w-12 lg:h-12 w-20 h-20 bg-blue-200" src="/joe.png" /></a></Link>
--- components/Layout.js
+++ components/Layout.js
@@ -3,14 +3,14 @@ import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
 
-export default function Layout({ children }) {
+export default function Layout({ children, transparentHeader = false }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Head>
         <title>Joe Attardi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
+      <Header transparent={transparentHeader} />
       <main className="flex-grow flex flex-col">
         {children}
       </main>
